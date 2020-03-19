6ffd30c835e5443d19067a0f9e42e60b115717eb
erikadudki <edudkin@uni-bonn.de>
2020-03-19 16:57:36 +0100

    add option sparsematrix

--- de_analysis/anndata_to_tsv.py
+++ de_analysis/anndata_to_tsv.py
@@ -3,9 +3,12 @@ import pandas as pd
 import anndata
 import os
 import warnings
+import scipy as sc
+
 
 wd = '/home/erika/PycharmProjects/DE-Analysis/src/code_tidy/'
-filename = 'de10;1'
+
+filename = 'Lung_sce'
 user_layer = 'logcounts'
 
 
@@ -17,6 +20,9 @@ def anndata_to_tsv(wd,
     """
     Transforms anndata (*.h5ad-file) to individual .tsv files for each patient
     for each cluster, which is the format needed for running the DE-Analysis.
+    Needs user input, during running the code, to set the parameters cl_input
+    (what is the columnname of the group of cluster-IDs) and s_input (what is
+    the columnname of the group for sample/patient-IDs)
 
     Input:
         wd: string
@@ -35,46 +41,92 @@ def anndata_to_tsv(wd,
     # TODO: add option for sparsematrix?
     adata = anndata.read_h5ad(wd + 'data/' + filename + '.h5ad')
     # which clusters/samples exist:
-    cl_id = adata.obs['cluster_id'].unique()
-    s_id = adata.obs['sample_id'].unique()
+    print(adata.obs.columns.values)
+    cl_input = input('Please enter: which of the IDs (showing on the previous '
+                     'line) belong to the cluster-ID? ')
+    # cl_input = 'cluster'
+    # cl_input = 'cluster_id'
+    cl_id = adata.obs[cl_input].unique()
+    s_input = input(
+        'Please enter: which of the IDs belongs to the sample-ID/patient-ID? ')
+    # s_input = 'group'
+    # s_input = 'sample_id'
+    s_id = adata.obs[s_input].unique()
 
     # for cl in range(1,nr_cl+1):
     for cl in cl_id:
         # subsets of celltypes
-        adata_cl = adata[adata.obs['cluster_id'] == cl]
+        adata_cl = adata[adata.obs[cl_input] == cl]
 
         # subset of samples/patients
         # for s in range(1,nr_s+1):
         for s in s_id:
-            adata_cl_s = adata_cl[adata_cl.obs['sample_id'] == s]
+            adata_cl_s = adata_cl[adata_cl.obs[s_input] == s]
 
             # transform to pandas-dataframe
-            # if sparsematrix:
-            #     pd_adata_cl_s = pd.DataFrame(data=np.transpose(adata_cl_s.X.toarray()),
-            #                                   index=adata_cl_s.var_names,
-            #                                   columns=adata_cl_s.obs_names)
-            #     if not os.path.exists(wd + 'data/' + 'data_per_pat_per_cl'):
-            #         os.mkdir(wd + 'data/' + 'data_per_pat_per_cl')
-            #
-            #     pd_adata_cl_s.to_csv(
-            #         wd + 'data/' + 'data_per_pat_per_cl' + '/cl' + str(cl) + 'Pt' + str(s))
-            # else:
-            if user_layer is None:
-                warnings.warn(
-                    "Type of normalized data (user_layer) is not specified. "
-                    "Please make sure, that your input data is normalized. "
-                    "Now, the main data matrix is considered.")
-                pd_adata_cl_s = pd.DataFrame(data=np.transpose(adata_cl_s.X),
-                                             columns= adata_cl_s.obs_names,
-                                             index=adata_cl_s.var_names)
+            # If data is in sparsematrix format csc
+            if sc.sparse.isspmatrix_csc(adata_cl_s.layers[user_layer]) or sc.sparse.isspmatrix_csr(adata_cl_s.layers[user_layer]):
+                if user_layer is None:
+                    warnings.warn(
+                        "Type of normalized data (user_layer) is not specified. "
+                        "Please make sure, that your input data is normalized. "
+                        "Now, the main data matrix is considered.")
+                    pd_adata_cl_s = pd.DataFrame(
+                        data=np.transpose(adata_cl_s.X.toarray()),
+                        columns= adata_cl_s.obs_names,
+                        index=adata_cl_s.var_names)
+                else:
+                    pd_adata_cl_s = pd.DataFrame(
+                        data=np.transpose(adata_cl_s.layers[user_layer].toarray()),
+                        index=adata_cl_s.var_names,
+                        columns=adata_cl_s.obs_names)
+                    if not os.path.exists(wd + 'data/' ):
+                        os.mkdir(wd + 'data/')
+
+            elif type(adata_cl_s.layers[user_layer]) == anndata._core.views.ArrayView:
+                if user_layer is None:
+                    warnings.warn(
+                        "Type of normalized data (user_layer) is not specified. "
+                        "Please make sure, that your input data is normalized. "
+                        "Now, the main data matrix is considered.")
+                    pd_adata_cl_s = pd.DataFrame(
+                        data=np.transpose(adata_cl_s.X),
+                        columns=adata_cl_s.obs_names,
+                        index=adata_cl_s.var_names)
+                else:
+                    pd_adata_cl_s = pd.DataFrame(
+                        data=np.transpose(adata_cl_s.layers[user_layer]),
+                        columns=adata_cl_s.obs_names,
+                        index=adata_cl_s.var_names)
+
             else:
-                pd_adata_cl_s = pd.DataFrame(
-                    data=np.transpose(adata_cl_s.layers[user_layer]),
-                    columns=adata_cl_s.obs_names,
-                    index=adata_cl_s.var_names)
+                try:
+                    if user_layer is None:
+                        warnings.warn(
+                            "Type of normalized data (user_layer) is not specified. "
+                            "Please make sure, that your input data is normalized. "
+                            "Now, the main data matrix is considered.")
+                        pd_adata_cl_s = pd.DataFrame(data=np.transpose(adata_cl_s.X),
+                                                     columns= adata_cl_s.obs_names,
+                                                     index=adata_cl_s.var_names)
+                    else:
+                        pd_adata_cl_s = pd.DataFrame(
+                            data=np.transpose(adata_cl_s.layers[user_layer]),
+                            columns=adata_cl_s.obs_names,
+                            index=adata_cl_s.var_names)
+                except ValueError:
+                    warnings.warn("Maybe the dataformat is not understood. ")
+                    print(adata_cl_s.obs_names)
+                    warnings.warn('should correspond to the single cells, and ')
+                    print(adata_cl_s.var_names)
+                    warnings.warn('should correspond to the genes. If that is correct, '
+                          'then probably the data-format is not '
+                          'understood and not implemented yet. '
+                          'Please post an issue. ')
+                    pass
 
             # save the panda files for each patient for each cluster
             name_to_save = 'cl' + str(cl) + '_Pt' + str(s)
-            pd_adata_cl_s.to_csv(wd + 'data/' + filename
+            pd_adata_cl_s.to_csv(wd + 'data/anndata_to_tsv/' + filename
                                  + '_' + name_to_save + '.tsv', sep = '\t')
     return
