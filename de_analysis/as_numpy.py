16d4cdb090c3fdbf2bebabe33223b8408871ffc5
erikadudki <edudkin@uni-bonn.de>
2020-04-29 15:27:51 +0200

    as_numpy function and wilc test function

---
+++ de_analysis/as_numpy.py
@@ -0,0 +1,30 @@
+# import numpy as np
+# import pandas as pd
+from de_analysis import *
+
+def save_as_np(pd_original,
+               loc_to):
+    '''
+
+    :param pd_original: pandas Dataframe
+    :param loc_to: string
+            location to save
+    :return:
+    '''
+    pd_np = pd_original.to_numpy()
+    np.save(loc_to + '.npy', pd_np)
+    col = pd_original.columns.to_numpy()
+    indx = pd_original.index.to_numpy()
+    np.save(loc_to + '_col.npy', col)
+    np.save(loc_to + '_indx.npy', indx)
+
+    return
+
+
+def read_numpy_to_df(filename):
+    npload = np.load(filename + '.npy')
+    npcol = np.load(filename + '_col.npy', allow_pickle=True)
+    npindx = np.load(filename + '_indx.npy', allow_pickle=True)
+    pd_from_np = pd.DataFrame(data=npload, columns=npcol, index=npindx)
+
+    return pd_from_np
---
+++ de_analysis/main_wilc_test.py
@@ -0,0 +1,162 @@
+# import numpy as np
+# import scipy.stats
+# import math
+# import time
+from de_analysis import *
+
+
+def main_wilc_test(len_pat_control,
+                   len_pat_copd,
+                   patient_list,
+                   patient_list_nonzero,
+                   row_gene,
+                   patients_group1,
+                   patients_group2,
+                   pd_wilc,
+                   index_genes,
+                   ind_filtered_genes,
+                   pd_fc_all_cells,
+                   pd_fc_expr_cells,
+                   pd_fc_expr_cells_med
+                   ):
+    end1 = time.time()
+    num_cross_val = len_pat_control* len_pat_copd
+    #len(patients_group1) * len(patients_group2)
+
+    Wilc_score = np.zeros(num_cross_val)
+    Wilc_nonzero = np.zeros(num_cross_val)
+    Effectsz = np.zeros(num_cross_val)
+    pval = np.zeros(num_cross_val)
+    pval_nonzero = np.zeros(num_cross_val)
+    col_names = np.empty(num_cross_val, dtype=object)
+    fc_all_cells = np.zeros(num_cross_val)
+    fc_expr_cells = np.zeros(num_cross_val)
+    fc_expr_cells_med = np.zeros(num_cross_val)
+    #
+    run_idx = 0
+    for i_pat_copd in range(len_pat_control,
+                            len_pat_control + len_pat_copd):
+        for i_pat_ctl in range(0, len_pat_control):
+            #CHANGEHERE
+            # pval[run_idx], Wilc_score[run_idx] = \
+            Wilc_score[run_idx], pval[run_idx] = \
+                scipy.stats.ranksums(patient_list[i_pat_ctl],
+                                     patient_list[i_pat_copd])
+            n1 = len(patient_list[i_pat_ctl])
+            n2 = len(patient_list[i_pat_copd])
+            Effectsz[run_idx] = Wilc_score[run_idx] / (n1 * n2)
+            if len(patient_list_nonzero[i_pat_ctl]) == 0 or \
+                    len(patient_list_nonzero[i_pat_copd]) == 0:
+                Wilc_nonzero[run_idx] = np.nan
+                pval_nonzero[run_idx] = np.nan
+            else:
+                Wilc_nonzero[run_idx], pval_nonzero[
+                    run_idx] = scipy.stats.ranksums(
+                    patient_list_nonzero[i_pat_ctl],
+                    patient_list_nonzero[i_pat_copd])
+
+            fc_all_cells[run_idx] = math.log(
+                (np.mean(patient_list[i_pat_copd])+1) /
+                 (np.mean(patient_list[i_pat_ctl])+1), 2)
+
+            # fold change: mean over nonzero cells (only expressed cells)
+            a = patient_list[i_pat_copd]
+            b = patient_list[i_pat_ctl]
+            if len(a[np.nonzero(a)]) == 0 or len(b[np.nonzero(b)]) == 0:
+                fc_expr_cells[run_idx] = 0
+                fc_expr_cells_med[run_idx] = 0
+            else:
+                fc_expr_cells[run_idx] = math.log(
+                    (np.mean(a[np.nonzero(a)])+1) /
+                    (np.mean(b[np.nonzero(b)])+1), 2)
+                fc_expr_cells_med[run_idx] = math.log(
+                    (np.median(a[np.nonzero(a)])+1) /
+                    (np.median(b[np.nonzero(b)])+1), 2)
+
+            # write column names
+            if row_gene == 0:
+                col_names[run_idx] = patients_group1[i_pat_ctl] + '_vs_' + \
+                                     patients_group2[i_pat_copd - len_pat_control]
+            run_idx = run_idx + 1
+
+    end2 = time.time()
+
+    time_Wilcoxon = end2 - end1
+
+    min_max_wilc = np.zeros(2)
+
+    min_max_wilc[0] = np.min(Wilc_score)
+    min_max_wilc[1] = np.max(Wilc_score)
+    median_wilc_score = np.median(Wilc_score)
+    mean_wilc_score = np.mean(Wilc_score)
+    mean_p_wilc = np.mean(pval)
+    median_effect_size = np.median(Effectsz)
+    if np.isnan(Wilc_nonzero).any():
+        median_wilc_nonzero = np.nan
+    else:
+        median_wilc_nonzero = np.median(Wilc_nonzero)
+
+
+    # save Wilcoxon scores & fold change values for all tests
+    if row_gene == 0:
+        pd_wilc.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_wilc.columns = col_names
+        pd_wilc.iloc[row_gene] = Wilc_score
+
+        pd_fc_all_cells.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_fc_all_cells.columns = col_names
+        pd_fc_all_cells.iloc[row_gene] = fc_all_cells
+
+        pd_fc_expr_cells.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_fc_expr_cells.columns = col_names
+        pd_fc_expr_cells.iloc[row_gene] = fc_expr_cells
+
+        pd_fc_expr_cells_med.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_fc_expr_cells_med.columns = col_names
+        pd_fc_expr_cells_med.iloc[row_gene] = fc_expr_cells_med
+        # pd_wilc.to_csv(where_to_save + 'wilc_scores_' + ct + '_filteredGenes0_25')
+    else:
+        pd_wilc.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_wilc.iloc[row_gene] = Wilc_score
+
+        pd_fc_all_cells.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_fc_all_cells.iloc[row_gene] = fc_all_cells
+
+        pd_fc_expr_cells.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_fc_expr_cells.iloc[row_gene] = fc_expr_cells
+
+        pd_fc_expr_cells_med.rename(
+            index={row_gene: index_genes[ind_filtered_genes[row_gene]]},
+            inplace=True)
+        pd_fc_expr_cells_med.iloc[row_gene] = fc_expr_cells_med
+
+    results_main_wilc = {'min_max_wilc': min_max_wilc,
+                    'median_wilc_score': median_wilc_score,
+                    'mean_wilc_score': mean_wilc_score,
+                    'mean_p_wilc': mean_p_wilc,
+                    'median_effect_size': median_effect_size,
+                    'median_wilc_nonzero': median_wilc_nonzero,
+                    'time_Wilcoxon': time_Wilcoxon}
+    return pd_wilc, \
+           pd_fc_all_cells, \
+           pd_fc_expr_cells, \
+           pd_fc_expr_cells_med,\
+           results_main_wilc,\
+           num_cross_val, \
+           fc_expr_cells_med
+
+
